a5b5bcc1722c9fda578b0251b784fb200f6ff3ee
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-07-24 13:52:41 +0200

    added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  features {}
+  alias = "storage_account"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+} 
+
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
