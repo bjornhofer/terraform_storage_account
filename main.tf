83156d8e630c8a16feef4749540ddedb3164ed76
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-12-04 16:31:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier              = var.storage_account_account_tier
   account_replication_type  = var.storage_account_account_replication_type
   enable_https_traffic_only = true
-  tags                      = var.tags
 }
 
 resource "azurerm_storage_container" "storage_container" {
