0ad95f24378f842330f54a90e954ff635c351b8b
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-12-04 16:15:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,14 @@ data "azurerm_resource_group" "rg" {
   name = var.storage_account_resource_group_name
 }
 
-locals {
-  storage_account_name            = lower(length(var.storage_account_name_prefix) > 0 && length(var.storage_account_name_suffix) > 0 ? "${var.storage_account_name_prefix}${var.base_name}${var.storage_account_name_suffix}" : length(var.storage_account_name_suffix) > 0 ? "${var.base_name}${var.storage_account_name_suffix}" : length(var.storage_account_name_prefix) > 0 ? "${var.storage_account_name_prefix}${var.base_name}" : var.base_name)
-  storage_container_name          = lower(length(var.storage_container_name_prefix) > 0 && length(var.storage_container_name_suffix) > 0 ? "${var.storage_container_name_prefix}-${var.base_name}-${var.storage_container_name_suffix}" : length(var.storage_container_name_suffix) > 0 ? "${var.base_name}-${var.storage_container_name_suffix}" : length(var.storage_container_name_prefix) > 0 ? "${var.storage_container_name_prefix}-${var.base_name}" : var.base_name)
-  private_endpoint_name           = lower(length(var.private_endpoint_name_prefix) > 0 && length(var.private_endpoint_name_suffix) > 0 ? "${var.private_endpoint_name_prefix}-${var.base_name}-${var.private_endpoint_name_suffix}" : length(var.private_endpoint_name_suffix) > 0 ? "${var.base_name}-${var.private_endpoint_name_suffix}" : length(var.private_endpoint_name_prefix) > 0 ? "${var.private_endpoint_name_prefix}-${var.base_name}" : var.base_name)
-  private_service_connection_name = lower(length(var.private_service_connection_name_prefix) > 0 && length(var.private_service_connection_name_suffix) > 0 ? "${var.private_service_connection_name_prefix}-${var.base_name}-${var.private_service_connection_name_suffix}" : length(var.private_service_connection_name_suffix) > 0 ? "${var.base_name}-${var.private_service_connection_name_suffix}" : length(var.private_service_connection_name_prefix) > 0 ? "${var.private_service_connection_name_prefix}-${var.base_name}" : var.base_name)
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  suffix = [ "${var.storage_account_name_suffix}" ]
+  prefix = [ "${var.storage_account_name_prefix}" ]
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                      = local.storage_account_name
+  name                      = module.naming.storage_account.name
   resource_group_name       = data.azurerm_resource_group.rg.name
   location                  = length(var.storage_account_location) > 0 ? var.storage_account_location : data.azurerm_resource_group.rg.location
   account_kind              = var.storage_account_account_kind
@@ -21,7 +20,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name                  = local.storage_container_name
+  name                  = module.naming.storage_container.name
   container_access_type = "private"
   storage_account_name  = azurerm_storage_account.storage_account.name
 }
@@ -29,12 +28,12 @@ resource "azurerm_storage_container" "storage_container" {
 
 resource "azurerm_private_endpoint" "pep" {
   count               = var.private_endpoint_creation ? 1 : 0
-  name                = local.private_endpoint_name
+  name                = module.naming.private_endpoint.name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   subnet_id           = var.private_endpoint_subnet_id
   private_service_connection {
-    name                           = local.private_service_connection_name
+    name                           = module.naming.private_service_connection.name
     is_manual_connection           = false
     private_connection_resource_id = azurerm_storage_account.storage_account.id
     subresource_names              = ["blob"]
